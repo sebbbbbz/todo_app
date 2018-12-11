87c15c2a3ef64c1159cf66a44c04b71dcdf8393c
Sebastian <sebastianssp@gmail.com>
2018-12-11 21:56:41 +0800

    section header component added

--- screens/Todo.js
+++ screens/Todo.js
@@ -1,14 +1,68 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button, ScrollView } from 'react-native';
+import { StyleSheet, Text, View, Button, ScrollView, FlatList, SectionList } from 'react-native';
+
+const sections = [
+    {
+        id: 0,
+        title: 'Section 1',
+        data:
+            [
+                { id: 0, text: 'Item 1' },
+                { id: 1, text: 'Item 2' },
+                { id: 2, text: 'Item 3' },
+                { id: 3, text: 'Item 4' }
+            ]
+    },
+
+
+    { 
+        id: 1, 
+        title: 'Section 2', 
+        data: 
+            [
+                { id: 3, text: 'Item 4' }, 
+                { id: 4, text: 'Item 5' }
+            ] 
+    }
+];
 
 class TodoScreen extends React.Component {
- 
+
     static navigationOptions = {
         title: 'Todo',
       }; 
 
+    keyExtractor = (item, index) => item.id;
+
+    renderRow = ({ item }) =>
+      (<View style={styles.row}>
+        <Text>{item.text}</Text>
+      </View>);
+
+        renderSectionHeader = ({ section }) => (
+            <View style={styles.row}>
+                <Text>{section.title}</Text>
+            </View>
+        );
+
+        renderSectionHeader = ({ section }) => (
+            <View style={styles.header}>
+                <Text>{section.title}</Text>
+            </View>
+        );
+
     render() {
         return (
+            <SectionList
+                sections={sections}
+                renderItem={this.renderRow}
+                renderSectionHeader={this.renderSectionHeader}
+                keyExtractor={this.keyExtractor}
+            />
+        );
+    }
+}
+
             //<View style={{ flex: 1, flexDirection: 'column', flexWrap: 'wrap' }}>
                 //<View 
                     //style={{ 
@@ -19,17 +73,40 @@ class TodoScreen extends React.Component {
              //<View style={{ flex: 1, backgroundColor: 'lavenderblush' }} />
              //<View style={{ flex: 1, backgroundColor: 'lightcyan' }} />
           //</View>);
-            <ScrollView>
+            /* {<ScrollView>
                 <View style={styles.container1} />
                 <View style={styles.container2} />
                 <View style={styles.container3} />
                 <View style={styles.container4} />
-            </ScrollView>
-        );
-    }
-}
+            </ScrollView>} */
 
 const styles = StyleSheet.create({
+    row: {
+        padding: 15,
+        marginBottom: 5,
+        backgroundColor: 'skyblue'
+    },
+    header: {
+        padding: 15,
+        marginBottom: 5,
+        backgroundColor: 'pink'
+    },
+    // styledText: {
+    //     fontSize: 15,
+    //     fontWeight: 'bold',
+    //     textDecorationLine: 'underline',
+    //     backgroundColor: 'aliceblue',
+    //     marginBottom: 20,
+    //     padding: 30, 
+    // },
+    // styledText1: {
+    //     fontSize: 15,
+    //     fontWeight: 'bold',
+    //     textDecorationLine: 'underline',
+    //     backgroundColor: 'pink',
+    //     marginBottom: 20,
+    //     padding: 30, 
+    // },
     container1: {
         padding: 100,
         backgroundColor: 'blue'
